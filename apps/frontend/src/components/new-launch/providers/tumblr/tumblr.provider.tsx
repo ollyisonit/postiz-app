a8cf62767c98bad7fbcb8643f8364fde1d40dd70
Olly <olly@ollyglenn.com>
2026-01-06 20:06:37 -0800

    Added enable-reblogs checkbox

--- apps/frontend/src/components/new-launch/providers/tumblr/tumblr.provider.tsx
+++ apps/frontend/src/components/new-launch/providers/tumblr/tumblr.provider.tsx
@@ -9,11 +9,18 @@ import { useSettings } from '@gitroom/frontend/components/launches/helpers/use.v
 import { TumblrSettingsDto } from '@gitroom/nestjs-libraries/dtos/posts/providers-settings/tumblr.dto';
 import { TumblrTagsSelect } from './tumblr.tags.select';
 import { TumblrBlogSelect } from './tumblr.blog.select';
+import { Checkbox } from '@gitroom/react/form/checkbox';
 
 const TumblrComponent: FC = () => {
   const form = useSettings();
   return (
-    <div>
+    <div className="flex flex-col">
+      <Checkbox
+        label="Enable Reblogs"
+        className="mb-5"
+        {...form.register('enable_reblogs', {value: true})}
+        >
+      </Checkbox>
       <TumblrBlogSelect {...form.register('blog')} />
       <TumblrTagsSelect label="Tags" {...form.register('tags')} />
     </div>
